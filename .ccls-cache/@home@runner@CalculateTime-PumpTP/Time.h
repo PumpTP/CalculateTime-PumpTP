8a6ceb8f4e07c1933c5c273080db927da599893c
PumpTP <132637433+PumpTP@users.noreply.github.com>
2023-05-12 05:09:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CalculateTime-PumpTP/Time.h
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+int getTime(int &h, int &m){
+  cout<<"Input hour & min:";
+  cin>> h >> m;
+  return 0;
+}
+
+int subtract(int h1, int m1, int h2, int m2){
+  int h = h2-h1;
+  if (h<0){
+    h=h*(-1);
+  }
+  int m = m2-m1;
+  if (m<0){
+    h--;
+  }
+  return h;
+}
+
+int subtract(int m1, int m2){
+  int m = m2-m1;
+  if (m<0){
+    m=m+60
+  }
+  return m;
+}
